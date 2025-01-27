4cba2e648449cfac64ea7ba4dc01a0a673514d08
Aether <2085163736@qq.com>
2025-01-27 14:21:22 +0800

    update files

--- Tree/BinarySearch.cpp
+++ Tree/BinarySearch.cpp
@@ -6,23 +6,32 @@ using namespace std;
 vector<int>arr = {56,91,62,31,13,48,20,4,81,17,34,58,61,6,53,57,44,75,83,69};
 
 int search(int begin,int end,int num) {
+    if(begin > end) {
+        return -1;
+    }
     int mid = (begin + end)/2;
     if(arr[mid] == num) {
         return mid;
     }
     else if(arr[mid]>num) {
-        search(begin,mid,num);
+        return search(begin,mid-1,num);
     }
-    else{
-        search(mid+1,end,num);
+    else {
+        return search(mid+1,end,num);
     }
-    return -1;
 }
 
 int main() {
     sort(arr.begin(),arr.end());
-    cout<<search(0,arr.size()-1,17);
-
+    for(auto i : arr) {
+        cout<<i<<endl;
+    }
+    cout<<search(0,arr.size()-1,17)<<endl;
+    cout<<search(0,arr.size()-1,17)<<endl;
+    cout<<search(0,arr.size()-1,4)<<endl;
+    cout<<search(0,arr.size()-1,70)<<endl;
+    cout<<search(0,arr.size()-1,0)<<endl;
+    cout<<search(0,arr.size()-1,58)<<endl;
 
     return 0;
 }
